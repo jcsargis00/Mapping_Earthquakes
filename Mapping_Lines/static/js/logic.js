bf08e02ee5432a84d7e92136ead0aecdc282d478
jcsargis00 <79231870+jcsargis00@users.noreply.github.com>
2022-03-25 12:32:57 -0700

    Update logic.js

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,7 +3,22 @@ console.log("working");
 // Create our initial map object
 // Set the longitude, latitude, and the starting zoom level
 
-var map = L.map('map').setView([40.7, -94.5], 4);
+var map = L.map('map').setView([30.1975, -97.6664], 5);
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [37.6213, -122.3790],
+    [30.1975, -97.6664],
+    [43.6777, -79.6248],
+    [40.6413, -73.7781]
+  ];
+
+  // Create a polyline using the line coordinates and make the line blue.
+L.polyline(line, {
+    color: "blue"
+    
+  }).addTo(map);
+
 // Add a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
